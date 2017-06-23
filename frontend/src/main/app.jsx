44d4c8853872d9d87e3eb7e9be8e2978cb1b04d7
Tayane Fernandes <tayane.rmf@gmail.com>
2017-06-23 11:46:54 -0300

    Add sidebar

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,8 +1,11 @@
 import '../common/template/dependencies'
 import React from 'react'
+import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar'
 
 export default props => (
   <div className='wrapper'>
-    <h1>App</h1>
+    <Header />
+    <SideBar />
   </div>
 )
