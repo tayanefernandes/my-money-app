044ba3eb876570965adfcc9b8e8fe65434a24b08
Tayane Fernandes <tayane.rmf@gmail.com>
2017-06-28 14:17:48 -0300

    Add redux-form

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,10 +1,17 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+import { reducer as toastrReducer} from 'react-redux-toastr'
+
 import DashboardReducer from '../dashboard/DashboardReducer'
 import TabReducer from '../common/tab/TabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
   dashboard: DashboardReducer,
-  tab: TabReducer
+  tab: TabReducer,
+  billingCycle: BillingCycleReducer,
+  form: formReducer,
+  toastr: toastrReducer
 })
 
 export default rootReducer
