2b508f3353f0a468c052b65989cc02e84bfdfc5b
Tayane Fernandes <tayane.rmf@gmail.com>
2017-06-29 16:17:04 -0300

    Add debt and credit list and summary

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -17,47 +16,47 @@ import Form from './billingCycleForm'
 
 class BillingCycle extends Component {
 
-  componentWillMount() {
-    this.props.init()
-  }
+    componentWillMount() {
+        this.props.init()
+    }
 
-  render() {
-    return (
-      <div>
-        <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
-        <Content>
-          <Tabs>
-            <TabsHeader>
-              <TabHeader label='Listar' icon='bars' target='tabList' />
-              <TabHeader label='Incluir' icon='plus' target='tabCreate' />
-              <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
-              <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
-            </TabsHeader>
-            <TabsContent>
-              <TabContent id='tabList'>
-                <h1>Lista</h1>
-                <List/>
-              </TabContent>
-              <TabContent id='tabCreate'>
-                <Form onSubmit={this.props.create} submitLabel='Incluir' submitClass='primary'/>
-              </TabContent>
-              <TabContent id='tabUpdate'>
-                <Form onSubmit={this.props.update} submitLabel='Alterar' submitClass='info'/>
-              </TabContent>
-              <TabContent id='tabDelete'>
-                <Form onSubmit={this.props.remove} readOnly={true} submitLabel='Excluir' submitClass='danger'/>
-              </TabContent>
-            </TabsContent>
-          </Tabs>
-        </Content>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList' />
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent>
+                            <TabContent id='tabList'>
+                                <List />
+                            </TabContent>
+                            <TabContent id='tabCreate'>
+                                <Form onSubmit={this.props.create}
+                                    submitLabel='Incluir' submitClass='primary' />
+                            </TabContent>
+                            <TabContent id='tabUpdate'>
+                                <Form onSubmit={this.props.update}
+                                    submitLabel='Alterar' submitClass='info' />
+                            </TabContent>
+                            <TabContent id='tabDelete'>
+                                <Form onSubmit={this.props.remove} readOnly={true}
+                                    submitLabel='Excluir' submitClass='danger' />
+                            </TabContent>
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
 }
 
-const mapDispatchToProps = dispatch => (
-  bindActionCreators({init, create, update, remove}
-    , dispatch)
-)
-
+const mapDispatchToProps = dispatch => bindActionCreators({
+    init, create, update, remove
+}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
