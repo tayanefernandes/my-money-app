dbc4d13ada08610e7f43bd19288909a4bc8ede03
Tayane Fernandes <tayane.rmf@gmail.com>
2017-06-29 16:46:28 -0300

    Fix layout

--- frontend/src/common/template/menuItem.jsx
+++ frontend/src/common/template/menuItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default props => (
-  <li>
-    <a href={props.path}>
-      <i className={`fa fa-${props.icon}`}></i> {props.label}
-    </a>
-  </li>
+    <li>
+        <a href={props.path}>
+            <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
+        </a>
+    </li>
 )
